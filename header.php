05bbe9cd1c25d3e078f5845057a1fda92ac772ce
Fred Wenzel <fwenzel@mozilla.com>
2008-09-28 21:46:41 -0400

    mark current page as active in top-left menu

--- header.php
+++ header.php
@@ -27,9 +27,9 @@
 		<ul>
 			<li <?php if (is_home()) echo 'class="active"';?>><a href="<?php bloginfo('url');?>/">home</a></li>
 			<li><a href="http://flickr.com/freeed">photos</a></li>
-			<!--<li <?php if (is_page('projects'));?>><a href="<?php bloginfo('url');?>/projects">my projects</a></li>-->
-			<li <?php if (is_page('about-me'));?>><a href="<?php bloginfo('url');?>/about-me">about</a></li>
-			<li <?php if (is_page('impressum'));?>><a href="<?php bloginfo('url');?>/impressum">impressum</a></li>
+			<!--<li <?php if (is_page('projects')) echo 'class="active"';?>><a href="<?php bloginfo('url');?>/projects">my projects</a></li>-->
+			<li <?php if (is_page('about-me')) echo 'class="active"';?>><a href="<?php bloginfo('url');?>/about-me">about</a></li>
+			<li <?php if (is_page('impressum')) echo 'class="active"';?>><a href="<?php bloginfo('url');?>/impressum">impressum</a></li>
 		</ul>
 	</div>
 </div>
